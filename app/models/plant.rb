d483ba41f99d1ce51150fd06fa455d735820b97f
alex-magana <alex.magana@andela.com>
2019-10-07 04:02:22 +0300

    Specify associations for model Plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,7 @@
 class Plant < ApplicationRecord
+  has_many :specialties, dependent: :destroy
+  has_many :vehicles, through: :specialties
+
+  validates_presence_of :name, :latitude, :longitude,
+                        :opening_time, :closing_time
 end
