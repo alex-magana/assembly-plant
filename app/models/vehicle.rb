68219ca581408f7e1bcda145779b933e61896dc0
alex-magana <alex.magana@andela.com>
2019-10-07 04:02:51 +0300

    Specify associations for model Vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,6 @@
 class Vehicle < ApplicationRecord
+  has_many :specialties, dependent: :destroy
+  has_many :plants, through: :specialties
+
+  validates_presence_of :name, :description, :price
 end
